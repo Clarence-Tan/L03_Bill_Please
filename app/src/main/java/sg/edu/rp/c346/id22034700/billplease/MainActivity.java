562c8a21776bc4ad6812489b7d29a681d128a848
Clarence-Tan <22034700@myrp.edu.sg>
2023-05-08 18:19:41 +0800

    Final

--- app/src/main/java/sg/edu/rp/c346/id22034700/billplease/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22034700/billplease/MainActivity.java
@@ -46,24 +46,58 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String show = "Total Bill: $";
                 String show2 = "Each Pays: $";
-                float floatAmt = Float.valueOf(amount.getText().toString());
+                double doubleAmt = Double.valueOf(amount.getText().toString());
                 int intPax = Integer.valueOf(pax.getText().toString());
-                float floatDiscount = Float.valueOf(discount.getText().toString());
-                    int checkedRadioId = rgPay.getCheckedRadioButtonId();
-                    float finalAmt = ((floatAmt * ((100 - floatDiscount)/100)));
-                    String output = String.format("%.2f", finalAmt);
-                    show = show + output;
-                    display.setText(show);
+                double doubleDiscount = Double.valueOf(discount.getText().toString());
+
+                if(SVS.isChecked()){
+                    double SVSDecimal = 1.10;
+                }
+
+                if(GST.isChecked()){
+                    double GSTDecimal = 1.07;
+                }
+
+                if((SVS.isChecked())&&(GST.isChecked())){
+                    double bothDecimal = 1.17;
+                }
+
+                int checkedRadioId = rgPay.getCheckedRadioButtonId();
+                double finalAmt = ((doubleAmt * ((100 - doubleDiscount)/100)));
+                if((SVS.isChecked())&&(GST.isChecked())){
+                    finalAmt *= 1.17;
+                } else if (SVS.isChecked()) {
+                    finalAmt *= 1.10;
+                } else if (GST.isChecked()) {
+                    finalAmt *= 1.07;
+                }
+                String output = String.format("%.2f", finalAmt);
+                show = show + output;
+                display.setText(show);
 
                     if (checkedRadioId == R.id.radioButtonCash){
                         String show3 = " in cash.";
-                        float eachAmtC = (((floatAmt * ((100 - floatDiscount)/100)))/intPax);
+                        double eachAmtC = (((doubleAmt * ((100 - doubleDiscount)/100)))/intPax);
+                        if((SVS.isChecked())&&(GST.isChecked())){
+                            eachAmtC *= 1.17;
+                        } else if (SVS.isChecked()) {
+                            eachAmtC *= 1.10;
+                        } else if (GST.isChecked()) {
+                            eachAmtC *= 1.07;
+                        }
                         String output2 = String.format("%.2f", eachAmtC);
                         show2 = show2 + output2 + show3;
                         displayEach.setText(show2);
                     } else {
                         String show3 = " via PayNow to 91239876.";
-                        float eachAmtPN = (((floatAmt * ((100 - floatDiscount)/100)))/intPax);
+                        double eachAmtPN = (((doubleAmt * ((100 - doubleDiscount)/100)))/intPax);
+                        if((SVS.isChecked())&&(GST.isChecked())){
+                            eachAmtPN *= 1.17;
+                        } else if (SVS.isChecked()) {
+                            eachAmtPN *= 1.10;
+                        } else if (GST.isChecked()) {
+                            eachAmtPN *= 1.07;
+                        }
                         String output2 = String.format("%.2f", eachAmtPN);
                         show2 = show2 + output2 + show3;
                         displayEach.setText(show2);
